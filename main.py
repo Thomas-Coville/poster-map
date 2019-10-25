267ec2a2543a02ee482155e03a44347ee7840a19
Thomas-Coville <tcoville@mixgenius.com>
2019-10-24 23:26:39 -0400

    rollback

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 
-import api
+import src
 import config
 
 
 # Note: debug=True is enabled here to help with troubleshooting. You should
 # remove this in production.
-app = api.create_app(config, debug=True)
+app = src.create_app(config, debug=True)
 
 
 # This is only used when running locally. When running live, gunicorn runs
