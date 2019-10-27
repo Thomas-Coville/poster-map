4b3eaa11e51d16681df9002868ca2f3fabb8848b
Thomas-Coville <tcoville@mixgenius.com>
2019-10-27 12:45:28 -0400

    chore: wip

--- posters/routes.py
+++ posters/routes.py
@@ -11,16 +11,10 @@ logger = logging.getLogger()
 
 # API
 @bp.route('/jobs', methods=['POST'])
-# @use_args(JobSchema())
-def create_job():
-    logger.info('FOOOO')
-    tasks.say_hello.delay('thomas')
-    pass
-    # service = JobsService()
+@use_args(JobSchema())
+def create_job(args):    
+    logger.info(f'task name is {tasks.say_hello.name}')
 
-    # job = service.create_job( latitude=args["latitude"],
-    #     longitude=args["longitude"])
-
-    # schema = JobSchema()
-    # return schema.dump(job)
+    result = tasks.say_hello.delay('thomas')
+    return "OK"
 
--- posters/tasks.py
+++ posters/tasks.py
@@ -1,6 +1,27 @@
 from celery import Celery
+from celery.signals import after_task_publish, before_task_publish
+
 
 celery = Celery(__name__, autofinalize=False)
+
+
+@after_task_publish.connect
+def task_sent_handler(sender=None, headers=None, body=None, **kwargs):
+    # information about task are located in headers for task messages
+    # using the task protocol version 2.
+    info = headers if 'task' in headers else body
+    print('after_task_publish for task id {info[id]}'.format(
+        info=info,
+    ))
+
+@before_task_publish.connect
+def task_sent_handler(sender=None, headers=None, body=None, **kwargs):
+    # information about task are located in headers for task messages
+    # using the task protocol version 2.
+    info = headers if 'task' in headers else body
+    print('after_task_publish for task id {info[id]}'.format(
+        info=info,
+    ))
 
 @celery.task(bind=True)
 def say_hello(self, dude):
